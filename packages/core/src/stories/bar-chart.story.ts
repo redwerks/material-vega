e62d7a16420338dfb8a58c59d65553fe38e2aa3a
Daniel Friesen <d@danf.ca>
2020-01-15 05:36:25 -0800

    Add Vega

--- packages/core/src/stories/bar-chart.story.ts
+++ packages/core/src/stories/bar-chart.story.ts
@@ -1,7 +1,35 @@
+import vegaEmbed from 'vega-embed';
+import { TopLevelSpec } from 'vega-lite';
+
 export default { title: 'Bar Chart' };
 
 export const Basic = (): HTMLElement => {
   const div = document.createElement('div');
-  div.textContent = 'Test';
+  const spec: TopLevelSpec = {
+    $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+    width: 200,
+    data: {
+      values: [
+        { quarter: 'Q1', percent: 0.2 },
+        { quarter: 'Q2', percent: 0.28 },
+        { quarter: 'Q3', percent: 0.32 },
+        { quarter: 'Q4', percent: 0.39 }
+      ]
+    },
+    mark: 'bar',
+    encoding: {
+      x: {
+        field: 'quarter',
+        type: 'nominal'
+      },
+      y: {
+        field: 'percent',
+        type: 'quantitative'
+      }
+    }
+  };
+
+  vegaEmbed(div, spec, { actions: false });
+
   return div;
 };
