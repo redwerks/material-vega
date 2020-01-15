39a4564536344f90cd6b16ed834a1380a2388de2
Daniel Friesen <d@danf.ca>
2020-01-15 05:27:00 -0800

    Fix storybook typescript

--- .storybook/html/main.js
+++ .storybook/html/main.js
@@ -1,4 +1,19 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../../packages/core/src/**/*.story.{js,jsx,ts,tsx}'],
-  addons: ['@storybook/preset-typescript']
+  addons: [
+    {
+      name: '@storybook/preset-typescript',
+      options: {
+        tsLoaderOptions: {
+          configFile: path.resolve(__dirname, '../../tsconfig.json')
+        },
+        tsDocgenLoaderOptions: {
+          tsconfigPath: path.resolve(__dirname, '../../tsconfig.json')
+        },
+        include: [path.resolve(__dirname, '../../packages/core')]
+      }
+    }
+  ]
 };
--- .storybook/react/main.js
+++ .storybook/react/main.js
@@ -1,4 +1,19 @@
+const path = require('path');
+
 module.exports = {
-  stories: ['../../packages/material-ui/src/**/*.story.{js,jsx,ts,tsx}'],
-  addons: ['@storybook/preset-typescript']
+  stories: ['../../packages/material-ui/src/**/*.story.{js,jsx,ts,tsx}']
+  addons: [
+    {
+      name: '@storybook/preset-typescript',
+      options: {
+        tsLoaderOptions: {
+          configFile: path.resolve(__dirname, '../../tsconfig.json')
+        },
+        tsDocgenLoaderOptions: {
+          tsconfigPath: path.resolve(__dirname, '../../tsconfig.json')
+        },
+        include: [path.resolve(__dirname, '../../packages/material-ui')]
+      }
+    }
+  ],
 };
