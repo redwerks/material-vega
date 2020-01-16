3ab67421b61b8486766ba3fa5ad56f212517b448
Daniel Friesen <d@danf.ca>
2020-01-15 21:19:08 -0800

    Simplify the code for example stories

---
+++ packages/embed/src/stories/_common.story.ts
@@ -0,0 +1,13 @@
+import { TopLevelSpec } from 'vega-lite';
+import vegaEmbed from '..';
+
+export const chart = (spec: TopLevelSpec) => (): HTMLElement => {
+  const div = document.createElement('div');
+
+  vegaEmbed(div, spec, {
+    actions: true,
+    renderer: 'svg'
+  });
+
+  return div;
+};
--- packages/embed/src/stories/bar-chart.story.ts
+++ packages/embed/src/stories/bar-chart.story.ts
@@ -1,91 +1,70 @@
-import vegaEmbed from '..';
-import { TopLevelSpec } from 'vega-lite';
+import { chart } from './_common.story';
 
 export default { title: 'Bar Chart' };
 
-export const Basic = (): HTMLElement => {
-  const div = document.createElement('div');
-  const spec: TopLevelSpec = {
-    $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
-    width: 200,
-    data: {
-      values: [
-        { quarter: 'Q1', percent: 0.2 },
-        { quarter: 'Q2', percent: 0.28 },
-        { quarter: 'Q3', percent: 0.32 },
-        { quarter: 'Q4', percent: 0.39 }
-      ]
-    },
-    mark: 'bar',
-    encoding: {
-      x: {
-        field: 'quarter',
-        type: 'nominal',
-        axis: {
-          ticks: false,
-          title: null
-        },
-        band: 0.5
+export const Basic = chart({
+  $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+  width: 200,
+  data: {
+    values: [
+      { quarter: 'Q1', percent: 0.2 },
+      { quarter: 'Q2', percent: 0.28 },
+      { quarter: 'Q3', percent: 0.32 },
+      { quarter: 'Q4', percent: 0.39 }
+    ]
+  },
+  mark: 'bar',
+  encoding: {
+    x: {
+      field: 'quarter',
+      type: 'nominal',
+      axis: {
+        ticks: false,
+        title: null
       },
-      y: {
-        field: 'percent',
-        type: 'quantitative',
-        axis: {
-          format: 'p',
-          orient: 'right',
-          title: null
-        }
+      band: 0.5
+    },
+    y: {
+      field: 'percent',
+      type: 'quantitative',
+      axis: {
+        format: 'p',
+        orient: 'right',
+        title: null
       }
     }
-  };
+  }
+});
 
-  vegaEmbed(div, spec, {
-    actions: true,
-    renderer: 'svg'
-  });
-
-  return div;
-};
-
-export const Horizontal = (): HTMLElement => {
-  const div = document.createElement('div');
-  const spec: TopLevelSpec = {
-    $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
-    height: 150,
-    data: {
-      values: [
-        { quarter: 'Q1', percent: 0.2 },
-        { quarter: 'Q2', percent: 0.28 },
-        { quarter: 'Q3', percent: 0.32 },
-        { quarter: 'Q4', percent: 0.39 }
-      ]
-    },
-    mark: 'bar',
-    encoding: {
-      x: {
-        field: 'percent',
-        type: 'quantitative',
-        axis: {
-          format: 'p',
-          title: null
-        }
-      },
-      y: {
-        field: 'quarter',
-        type: 'nominal',
-        axis: {
-          orient: 'left',
-          title: null
-        },
-        band: 0.75
+export const Horizontal = chart({
+  $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+  height: 150,
+  data: {
+    values: [
+      { quarter: 'Q1', percent: 0.2 },
+      { quarter: 'Q2', percent: 0.28 },
+      { quarter: 'Q3', percent: 0.32 },
+      { quarter: 'Q4', percent: 0.39 }
+    ]
+  },
+  mark: 'bar',
+  encoding: {
+    x: {
+      field: 'percent',
+      type: 'quantitative',
+      axis: {
+        format: 'p',
+        title: null
       }
+    },
+    y: {
+      field: 'quarter',
+      type: 'nominal',
+      axis: {
+        orient: 'left',
+        title: null
+      },
+      band: 0.75
     }
-  };
-
-  vegaEmbed(div, spec, {
-    actions: true,
-    renderer: 'svg'
-  });
-
-  return div;
-};
+  }
+});
