5b36acd846a2455e5cf6601c808f2fb2ccb8bd24
Daniel Friesen <d@danf.ca>
2020-01-16 22:25:10 -0800

    Move eslint config to packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,54 +2,15 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es6: true,
     node: true
   },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:eslint-comments/recommended',
-    'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
-    'prettier/react',
-    'plugin:jest/recommended'
+    '@redwerks/eslint-config',
+    '@redwerks/eslint-config-typescript',
+    '@redwerks/eslint-config-react',
+    '@redwerks/eslint-config-jest/tests-folder'
   ],
-  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
     tsconfigRootDir: __dirname
-  },
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
-  plugins: ['@typescript-eslint', 'jest'],
-  rules: {
-    'eslint-comments/no-unused-disable': 'warn',
-    'eslint-comments/disable-enable-pair': [
-      'error',
-      {
-        allowWholeFile: true
-      }
-    ],
-    'prettier/prettier': 'warn'
-  },
-  overrides: [
-    {
-      files: ['__tests__'],
-      env: {
-        'jest/globals': true
-      },
-      rules: {
-        'jest/consistent-test-it': [
-          'error',
-          {
-            fn: 'it'
-          }
-        ]
-      }
-    }
-  ]
+  }
 };
