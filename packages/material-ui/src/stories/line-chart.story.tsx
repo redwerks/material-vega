323a9839303c49636d8afd4ae25e6bf6d01a4bd7
Daniel Friesen <d@danf.ca>
2020-04-07 16:37:04 -0700

    Add example with individually labeled lines

--- packages/material-ui/src/stories/line-chart.story.tsx
+++ packages/material-ui/src/stories/line-chart.story.tsx
@@ -24,3 +24,48 @@ export const Basic = (): ReactElement => (
     }}
   />
 );
+
+export const LabeledLines = (): ReactElement => (
+  <MuiVegaLite
+    variant="lineChart"
+    spec={{
+      $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+      description: 'Multi-series line chart with labels.',
+      data: {
+        url: 'https://vega.github.io/vega-lite/examples/data/stocks.csv'
+      },
+      transform: [{ filter: "datum.symbol!=='IBM'" }],
+      encoding: { color: { field: 'symbol', type: 'nominal', legend: null } },
+      layer: [
+        {
+          mark: 'line',
+          encoding: {
+            x: { field: 'date', type: 'temporal', title: 'date' },
+            y: { field: 'price', type: 'quantitative', title: 'price' }
+          }
+        },
+        {
+          encoding: {
+            x: { aggregate: 'max', field: 'date', type: 'temporal' },
+            y: {
+              aggregate: { argmax: 'date' },
+              field: 'price',
+              type: 'quantitative',
+              axis: {
+                orient: 'left',
+                labelAlign: 'right'
+              }
+            }
+          },
+          layer: [
+            { mark: { type: 'circle' } },
+            {
+              mark: { type: 'text', align: 'left', dx: 4 },
+              encoding: { text: { field: 'symbol', type: 'nominal' } }
+            }
+          ]
+        }
+      ]
+    }}
+  />
+);
