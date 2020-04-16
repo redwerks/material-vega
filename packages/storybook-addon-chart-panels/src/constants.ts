3f8b652b18639e802fd0b296b76b22521815c7e1
Daniel Friesen <d@danf.ca>
2020-04-15 22:05:00 -0700

    Move Storybook addons to a separate repository

--- packages/storybook-addon-chart-panels/src/constants.ts
+++
@@ -1,5 +0,0 @@
-export const ADDON_ID = 'material-vega/storybook-addon-chart-panels';
-export const ADDON_PANEL = `${ADDON_ID}/panel`;
-
-const CHART = `${ADDON_ID}/chart`;
-export const EVENTS = { CHART };
--- packages/storybook-addon-preview-original/src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const ADDON_ID = 'material-vega/storybook-addon-preview-original';
-export const ADDON_PANEL = `${ADDON_ID}/panel`;
