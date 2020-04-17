65953635fb6ec5f847d1a60902a746fdb303c73f
Daniel Friesen <d@danf.ca>
2020-04-16 21:16:31 -0700

    Add docs addon to Material UI

--- .storybook/material-ui/main.js
+++ .storybook/material-ui/main.js
@@ -1,5 +1,5 @@
 const createMain = require('../common/main');
 
 module.exports = createMain('material-ui', {
-  addons: ['@storybook/addon-contexts/register']
+  addons: ['@storybook/addon-contexts', '@storybook/addon-docs']
 });
