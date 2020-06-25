d62c2e3e96de45820c8d54124e4094c5aeba1443
Daniel Friesen <d@danf.ca>
2020-06-25 14:59:11 -0700

    Add ref passthrough to Vega component

--- packages/material-ui/src/MuiVega.tsx
+++ packages/material-ui/src/MuiVega.tsx
@@ -1,17 +1,18 @@
 import React, {
-  FC,
   useReducer,
   useCallback,
   useRef,
   memo,
   Reducer,
-  ComponentType
+  ComponentType,
+  forwardRef,
+  Ref
 } from 'react';
 import { useMemo } from 'react';
 import { mergeConfig, TooltipHandler, Item } from 'vega';
 import { Vega } from 'react-vega';
 import { VegaProps } from 'react-vega/lib/Vega';
-import { useTheme } from '@material-ui/core';
+import { useTheme, useForkRef } from '@material-ui/core';
 import {
   createTheme,
   ThemeTypes,
@@ -49,78 +50,82 @@ interface TooltipReducerAction {
 /**
  * <Vega> from react-vega but with a Material UI integrated theme
  */
-export const MuiVega: FC<MuiVegaProps> = props => {
-  const {
-    variant,
-    color = 'primary',
-    background = 'transparent',
-    tooltip: tooltipProp,
-    TooltipComponent = MuiVegaTooltip,
-    config: configProp,
-    themeOptions,
-    ...vegaProps
-  } = props;
-  const theme = useTheme<MuiVegaTheme>();
-  const { offsetX, offsetY } = {
-    ...defaultVegaTooltipOptions,
-    ...theme.vegaTooltip
-  };
+export const MuiVega = forwardRef(
+  (props: MuiVegaProps, parentRef: Ref<Vega>) => {
+    const {
+      variant,
+      color = 'primary',
+      background = 'transparent',
+      tooltip: tooltipProp,
+      TooltipComponent = MuiVegaTooltip,
+      config: configProp,
+      themeOptions,
+      ...vegaProps
+    } = props;
+    const theme = useTheme<MuiVegaTheme>();
+    const { offsetX, offsetY } = {
+      ...defaultVegaTooltipOptions,
+      ...theme.vegaTooltip
+    };
 
-  if (typeof configProp === 'string') {
-    throw new TypeError(
-      '@material-vega/material-ui does not support a string `config`.'
+    if (typeof configProp === 'string') {
+      throw new TypeError(
+        '@material-vega/material-ui does not support a string `config`.'
+      );
+    }
+
+    const vegaRef = useRef();
+    const ref = useForkRef(vegaRef, parentRef);
+
+    const [tooltipState, dispatchTooltip] = useReducer<
+      Reducer<TooltipState | null, TooltipReducerAction>
+    >((state, { event, value }) => {
+      if (value == null || value === '') return null;
+
+      const { x, y } = calculatePosition(
+        event,
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (vegaRef.current as any).vegaEmbed.current.containerRef.current.getBoundingClientRect(),
+        offsetX,
+        offsetY
+      );
+
+      return { position: { x, y }, value };
+    }, undefined);
+
+    const tooltipHandler = useCallback<TooltipHandler>(
+      (handler, event, item, value): void => {
+        dispatchTooltip({ handler, event, item, value });
+      },
+      []
+    );
+
+    const tooltip = tooltipProp || tooltipHandler;
+
+    const muiThemeOptions = useMuiVegaOptions({ color, background });
+    const config = useMemo(
+      () =>
+        mergeConfig(
+          createTheme({ ...muiThemeOptions, ...themeOptions }, variant),
+          configProp || {}
+        ),
+      [theme, color, themeOptions]
+    );
+
+    return (
+      <>
+        <VegaMemo
+          ref={ref}
+          config={config}
+          tooltip={tooltip}
+          renderer="svg"
+          {...vegaProps}
+        />
+        {tooltipState !== undefined && (
+          <TooltipComponent value={null} {...tooltipState} />
+        )}
+      </>
     );
   }
-
-  const ref = useRef();
-
-  const [tooltipState, dispatchTooltip] = useReducer<
-    Reducer<TooltipState | null, TooltipReducerAction>
-  >((state, { event, value }) => {
-    if (value == null || value === '') return null;
-
-    const { x, y } = calculatePosition(
-      event,
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      (ref.current as any).vegaEmbed.current.containerRef.current.getBoundingClientRect(),
-      offsetX,
-      offsetY
-    );
-
-    return { position: { x, y }, value };
-  }, undefined);
-
-  const tooltipHandler = useCallback<TooltipHandler>(
-    (handler, event, item, value): void => {
-      dispatchTooltip({ handler, event, item, value });
-    },
-    []
-  );
-
-  const tooltip = tooltipProp || tooltipHandler;
-
-  const muiThemeOptions = useMuiVegaOptions({ color, background });
-  const config = useMemo(
-    () =>
-      mergeConfig(
-        createTheme({ ...muiThemeOptions, ...themeOptions }, variant),
-        configProp || {}
-      ),
-    [theme, color, themeOptions]
-  );
-
-  return (
-    <>
-      <VegaMemo
-        ref={ref}
-        config={config}
-        tooltip={tooltip}
-        renderer="svg"
-        {...vegaProps}
-      />
-      {tooltipState !== undefined && (
-        <TooltipComponent value={null} {...tooltipState} />
-      )}
-    </>
-  );
-};
+);
+MuiVega.displayName = 'MuiVega';
--- packages/material-ui/src/MuiVegaLite.tsx
+++ packages/material-ui/src/MuiVegaLite.tsx
@@ -1,11 +1,15 @@
-import React, { FC } from 'react';
+import React, { forwardRef, Ref } from 'react';
 import { MuiVega, MuiVegaProps } from './MuiVega';
+import { Vega } from 'react-vega';
 
 export type MuiVegaLiteProps = Omit<MuiVegaProps, 'mode'>;
 
 /**
  * <VegaLite> from react-vega but with a Material UI integrated theme
  */
-export const MuiVegaLite: FC<MuiVegaLiteProps> = props => {
-  return <MuiVega {...props} mode="vega-lite" />;
-};
+export const MuiVegaLite = forwardRef(
+  (props: MuiVegaLiteProps, ref: Ref<Vega>) => {
+    return <MuiVega ref={ref} {...props} mode="vega-lite" />;
+  }
+);
+MuiVegaLite.displayName = 'MuiVegaLite';
